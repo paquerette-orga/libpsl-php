2cc0c0e4514f0eee6770385d80c6a5e150b918c9
frju365 <abld@abld.info>
2019-03-06 16:10:47 +0100

    test

--- test/test1.php
+++ test/test1.php
@@ -1,7 +1,7 @@
 <?php
-require("/home/julien/Documents/Dev/Sys/libpsl-php/psl-inspector.php");
+require("psl-inspector.php");
 
-$psl_file = "/home/julien/Documents/Dev/Sys/libpsl-php/test/test.dat";
+$psl_file = "test/test.dat";
 $psl_test = new CheckTLD($psl_file, "test@test.com");
 
 $hello = $psl_test->CheckTLD();
