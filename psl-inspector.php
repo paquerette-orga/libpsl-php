6133a35c64e318803f28bcf9826fa0bffddd848c
frju365 <abld@abld.info>
2019-03-06 17:47:25 +0100

    Version 0.2

--- psl-inspector.php
+++ psl-inspector.php
@@ -7,7 +7,7 @@ class CheckTLD
 {
   private $psl_file;
   private $host;
-  
+
   public function __construct($psl_file, $host = "")
   {
     $this->psl_file = $psl_file;
@@ -28,6 +28,8 @@ class CheckTLD
     $host = $this->host;
 
     $verify = 0;
+    $pverify = 1;
+    $prefix = "";
 
 
     // Shift the address to lowercase to simplify checking
@@ -36,23 +38,29 @@ class CheckTLD
 
     // Split the Address into user and domain parts
 
-    $UD = explode("@", $host);
-
-    if (sizeof($UD) <= 1) {
-      $UD = explode(".", $host);
-    }
-    if (sizeof($UD) <= 1) {
+    $Domain = explode("@", $host);
+    $Domain = explode(".", $host);
+    if (sizeof($Domain) <= 1) {
       echo "Error";
     }
 
-
     // Split the domain into at least 2 parts
-    $Domain = explode(".", $UD[1]);
+    //$Domain = explode(".", $UD[$size - 1]);
     $sLevels = sizeof($Domain);
     if ($sLevels < 1) $verify = 1;
+    
     // Get the TLD, strip off trailing ] } ) > and check the length
 
     $tld = $Domain[$sLevels - 1];
+    echo $tld;
+
+    // Case the TLD is a prefix (more than 3 part)
+    if ($sLevels > 1) {
+      $prefix = $Domain[$sLevels - 2].".".$Domain[$sLevels - 1];
+      if ($prefix) {
+        $pverify = 0;
+      }
+    }
 
     if (strlen($tld) < 2 && $tld != "arpa") $verify = 1;
 
@@ -60,11 +68,18 @@ class CheckTLD
       $verify = 1;
     }
 
+    if (preg_match_all('/\n'.$prefix.'/', $psl_file, $resultat, PREG_PATTERN_ORDER) == FALSE) {
+      $pverify = 1;
+    }
+
     if($verify == 0) {
       echo "TLD has been checked and is a valid host (BY TLD)";
     }
     else {
       echo "Host provided has not a valid TLD";
+    }
+    if($pverify == 0) {
+      echo "\nHost is in the Prefix List";
     }
   }  
 }
--- test/test1.php
+++ test/test1.php
@@ -2,7 +2,7 @@
 require("psl-inspector.php");
 
 $psl_file = "test/test.dat";
-$psl_test = new CheckTLD($psl_file, "test@test.com");
+$psl_test = new CheckTLD($psl_file, "lio@test.noho.st");
 
 $hello = $psl_test->CheckTLD();
 
